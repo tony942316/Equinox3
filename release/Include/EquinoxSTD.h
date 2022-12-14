482db79ad10c3b73e3ea5f558e1f7e2f6156f971
tony942316 <grasso99@icloud.com>
2022-12-14 01:46:54 -0500

    testIntDirstribution Fix

---
+++ release/Include/EquinoxSTD.h
@@ -0,0 +1,4 @@
+#pragma once
+
+#include "Random.h"
+#include "Log.h"
---
+++ release/Include/Random.h
@@ -0,0 +1,50 @@
+#pragma once
+
+#include <random>
+
+namespace eqx
+{
+	/**
+	 * @brief Random Number Generator API
+	 */
+	class Random
+	{
+	public:
+		Random(const Random& r) = delete;
+		void operator =(const Random& r) = delete;
+
+		/**
+		 * @brief Generate A Random Integer
+		 * 
+		 * @param lowerBound Lowest Possible Value
+		 * @param upperBound Highest Possible Value
+		 * @returns Random Integer
+		 */
+		static int randInt(int lowerBound, int upperBound);
+
+		/**
+		 * @brief Generate A Random Double
+		 * 
+		 * @param lowerBound Lowest Possible Value
+		 * @param upperBound Highest Possible Value
+		 * @returns Random Double
+		*/
+		static double randDouble(double lowerBound, double upperBound);
+
+		/**
+		 * @brief Generate A Random Seed From Current Time
+		 * 
+		 * @returns Seed
+		*/
+		static unsigned int generateSeed();
+
+	private:
+		Random();
+
+		static Random& getInstance();
+
+		std::default_random_engine engine;
+		std::uniform_int_distribution<int> uniInt;
+		std::uniform_real_distribution<double> uniDouble;
+	};
+}
